fafba4cccc66c7edb2a06d192c51c237f89c30ff
Oliver Hellebusch <mail@olihel.com>
2013-04-29 20:01:02 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/*!
+ * {{title}}
+ * https://github.com/olihel/{{name}}.git
+ *
+ * Copyright (c) {{year}} Oliver Hellebusch
+ * Released under MIT license (http://olihel.github.io/{{name}}/LICENSE)
+ */
+
+console.log('index.js');
